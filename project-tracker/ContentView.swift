1b3e3ba9fc4b5eeb750ef98de759a87078498faf
Konneki <konnekdaniel@gmail.com>
2024-10-09 22:04:14 +0200

    Initial Commit

---
+++ project-tracker/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  project-tracker
+//
+//  Created by Daniel Konnek on 9.10.2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ project-tracker/project_trackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  project_trackerApp.swift
+//  project-tracker
+//
+//  Created by Daniel Konnek on 9.10.2024.
+//
+
+import SwiftUI
+
+@main
+struct project_trackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
